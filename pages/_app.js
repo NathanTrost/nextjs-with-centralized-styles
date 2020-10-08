db47d7265ff9bf739349a1dbc91fb9873a20fe8a
Nathan Trost <n.d.trost@gmail.com>
2020-10-08 11:29:20 -0500

    First commit

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "../styles/globalStyles.scss";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/categories/second-post.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+const SecondPost = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Second Post</title>
+      </Head>
+      <h1>Second Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+};
+
+export default SecondPost;
