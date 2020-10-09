f296170910657032eb263b426eab1b94b2560312
Nathan Trost <n.d.trost@gmail.com>
2020-10-09 13:01:03 -0500

    Bulma stuff

--- components/content.js
+++ components/content.js
@@ -1,6 +1,62 @@
+import classNames from "classnames";
+import styles from "./content.module.scss";
+
 const Content = () => {
-  const content = "";
-  return <>{content}</>;
+  const columnWithBg = classNames(["column", styles["col-bg"]]);
+  return (
+    <>
+      <h2 className="title is-2">Basic Grid</h2>
+      <div className={classNames(["container"])}>
+        <div className={classNames(["columns", styles["row-bg"]])}>
+          <div className={columnWithBg}>Column 1</div>
+          <div className={columnWithBg}>Column 2</div>
+          <div className={columnWithBg}>Column 3</div>
+          <div className={columnWithBg}>Column 4</div>
+          <div className={columnWithBg}>Column 5</div>
+          <div className={columnWithBg}>Column 6</div>
+          <div className={columnWithBg}>Column 7</div>
+          <div className={columnWithBg}>Column 8</div>
+          <div className={columnWithBg}>Column 9</div>
+          <div className={columnWithBg}>Column 10</div>
+        </div>
+        <div className={classNames(["columns", styles["row-bg"]])}>
+          <div className={columnWithBg}>Column 1</div>
+          <div className={columnWithBg}>Column 2</div>
+          <div className={columnWithBg}>Column 3</div>
+          <div className={columnWithBg}>Column 4</div>
+          <div className={columnWithBg}>Column 5</div>
+          <div className={columnWithBg}>Column 6</div>
+        </div>
+      </div>
+      <br />
+      <h2 className="title is-2">Table with form</h2>
+      <div className={classNames(["columns"])}>
+        <div className="column is-8">
+          <div className="field">
+            <label className="label">Text Input</label>
+            <input class="input" type="text" placeholder="Text input" />
+          </div>
+          <div className="field">
+            <label className="label">Select Input</label>
+            <div class="select">
+              <select>
+                <option>Select dropdown</option>
+                <option>With options</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="column is-4">
+          <div className="block">
+            <button class="button is-primary is-rounded">Reset</button>
+          </div>
+          <div className="block">
+            <button class="button is-primary is-rounded">Send</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Content;
--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,7 @@ export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
+    <div className={classNames(["container", styles.container])}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
