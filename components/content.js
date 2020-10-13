e2fd05bb74e754c3363a236f021762f7985808fb
Nathan Trost <n.d.trost@gmail.com>
2020-10-13 12:52:05 -0500

    Quick example

--- components/content.js
+++ components/content.js
@@ -1,7 +1,15 @@
 import styles from "content.module.scss";
 const Content = () => {
-  const content = "";
-  return <>{content}</>;
+  return (
+    <div class="md:flex bg-white rounded-lg p-6">
+      <div class="text-center md:text-left">
+        <h2 class="text-lg">Erin Lindford</h2>
+        <div class="text-purple-500">Product Engineer</div>
+        <div class="text-gray-600">erinlindford@example.com</div>
+        <div class="text-gray-600">(555) 765-4321</div>
+      </div>
+    </div>
+  );
 };
 
 export default Content;
