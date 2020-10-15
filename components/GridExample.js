bf1d39800dcef02bb63dd92f22d145e1f3dddfc5
Nathan Trost <n.d.trost@gmail.com>
2020-10-15 07:52:37 -0500

    Update pages

--- components/GridExample.js
+++ components/GridExample.js
@@ -1,43 +1,16 @@
 import FormGroup from "carbon-components-react/lib/components/FormGroup";
-import Form from "carbon-components-react/lib/components/Form";
+
 import { Grid, Column, Row } from "carbon-components-react/lib/components/Grid";
-import Select from "carbon-components-react/lib/components/Select";
-import SelectItem from "carbon-components-react/lib/components/SelectItem";
-import TextInput from "carbon-components-react/lib/components/TextInput";
-import Button from "carbon-components-react/lib/components/Button";
-import ButtonSet from "carbon-components-react/lib/components/ButtonSet";
 
 const GridExample = () => {
   return (
-    <Form>
-      <Grid>
-        <Row>
-          <Column>
-            <FormGroup legentText="Inputs">
-              <TextInput type="text" />
-              <Select id="select-1" defaultValue="placeholder-item">
-                <SelectItem
-                  disabled
-                  hidden
-                  value="placeholder-item"
-                  text="Choose an option"
-                />
-                <SelectItem value="option-1" text="Option 1" />
-                <SelectItem value="option-2" text="Option 2" />
-                <SelectItem value="option-3" text="Option 3" />
-              </Select>
-            </FormGroup>
-          </Column>
-
-          <Column>
-            <ButtonSet stacked={true}>
-              <Button kind="secondary">Reset</Button>
-              <Button kind="primary">Submit</Button>
-            </ButtonSet>
-          </Column>
-        </Row>
-      </Grid>
-    </Form>
+    <Grid>
+      <Row>
+        <Column>Column 1</Column>
+        <Column>Column 2</Column>
+        <Column>Column 3</Column>
+      </Row>
+    </Grid>
   );
 };
 
--- pages/categories/form.js
+++ pages/categories/form.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout from "../../layouts/layout";
+import FormExample from "../../components/FormExample";
 
 const FormPage = () => {
   return (
@@ -9,6 +9,7 @@ const FormPage = () => {
         <title>Second Post</title>
       </Head>
       <h1>Second Post</h1>
+      <FormExample />
     </Layout>
   );
 };
